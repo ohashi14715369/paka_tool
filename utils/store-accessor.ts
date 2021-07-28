25eaeba6ad374bc27f406682cefae1dc5d0af306
ohashi14715369 <ohashi14715369@gmail.com>
2021-07-28 22:47:36 +0900

    chore: gh-pages 用スクリプト、設定追加。 chore: eslint, stylint, prettier 関連の設定、実行。 feat: Jest, Axios 環境構築。 feat: nuxt-property-decorator, vuex-persistedstate, vuex-module-decorators の導入。

---
+++ utils/store-accessor.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-mutable-exports */
+import { Store } from 'vuex';
+// import { getModule } from 'vuex-module-decorators';
+function initialiseStores(_store: Store<any>): void {}
+
+export { initialiseStores };
